4ec1269730ea8dade955f3d3650881d5c83bb44e
Alex North <alex@alexn.id.au>
2012-03-10 23:48:45 +1100

    Factored out generator service

--- js/controllers.js
+++ js/controllers.js
@@ -1,77 +1,53 @@
-function GeneratorCtrl() {
+/* Generator */
+function GeneratorCtrl(generator) {
+  this.generator = generator;
+  this.functionName = "sine";
 }
 
-function AnalyserCtrl() {
-  this.bufferSize = 4096;
-  this.floor = -60;
-  this.audioContext = new webkitAudioContext();
-  this.sampleRate = this.audioContext.sampleRate;
-  this.fn = "(440*t%2<1)?440*t%1-0.5:0.5-(440*t%1)";
-  this.playing = false;
-  
-  // Calculate window, a Hanning Window
-  // TODO: consider a Kaiser-Bessel window
-  this.windowCoeff = new Array(this.bufferSize)
-  var mid = Math.floor(this.bufferSize / 2);
-  this.windowCoeff[0] = 0;
-  this.windowCoeff[mid] = 2;
-  for (var i = 0; i < mid; ++i) {
-    this.windowCoeff[mid + i] = 1 + Math.cos(Math.PI * i / mid);
-    this.windowCoeff[mid - i] = this.windowCoeff[mid + i]
-  }
+GeneratorCtrl.$inject = ['generator'];
 
-  // Create source
-  this.sourceNode = this.audioContext.createBufferSource();
-  this.sourceNode.loop = true;
-  this.sourceNode.buffer = this.audioContext.createBuffer(1, this.bufferSize, this.sampleRate);
-
-  // Create analyser
-  this.analyserNode = this.audioContext.createAnalyser();
-
-  this.samples = this.sourceNode.buffer.getChannelData(0);
-  this.spectrum = new Float32Array(this.analyserNode.frequencyBinCount);
-  
-  // Wire things up
-  this.sourceNode.connect(this.analyserNode, 0, 0);
-  this.analyserNode.connect(this.audioContext.destination, 0, 0);
-}
-
-
-AnalyserCtrl.prototype = {
-  onFunctionChanged: function() {
-    for (var x = 0; x < this.samples.length; ++x) {
-      var t = x / this.sampleRate;
-      try {
-        this.samples[x] = eval(this.fn) * this.windowCoeff[x];
-      } catch (e) {
-        this.samples[x] = 0;
-      }
-    }
+GeneratorCtrl.prototype = {
+  setFunction: function() {
+    this.generator.setFunction(this.functionName);
     this.drawWaveform();
   },
 
+  setFrequency: function() {
+    this.generator.frequency = this.frequency;
+    this.setFunction();
+  }
+};
+
+/* Analyzer */
+function AnalyserCtrl(generator) {
+  this.generator = generator;
+}
+
+AnalyserCtrl.$inject = ['generator'];
+
+AnalyserCtrl.prototype = {
   drawWaveform: function() {
    var canvas = document.getElementById("waveformCanvas");
    var ctx = canvas.getContext('2d');
    ctx.clearRect(0, 0, canvas.width, canvas.height);
    // Draw zero line
    var originY = canvas.height / 2;
-   var samplesPerPixel = this.samples.length / canvas.width;
+   var samplesPerPixel = this.generator.samples.length / canvas.width;
    ctx.fillRect(0, originY, canvas.width, 1);
    // Draw waveform
    ctx.beginPath();
    ctx.moveTo(0, originY);
    for (var x = 0; x < canvas.width; ++x) {
      var sampleIdx = Math.round(x * samplesPerPixel);
-     var y = originY - (this.samples[sampleIdx] * originY);
+     var y = originY - (this.generator.samples[sampleIdx] * originY);
      ctx.lineTo(x, y);
    }
    ctx.stroke();
   },
 
   drawSpectrum: function() {
-    if (this.playing) {
-      this.analyserNode.getFloatFrequencyData(this.spectrum);
+    if (this.generator.playing) {
+      this.generator.analyserNode.getFloatFrequencyData(this.generator.spectrum);
       var canvas = document.getElementById("spectrumCanvas");
       var ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -82,8 +58,8 @@ AnalyserCtrl.prototype = {
       ctx.scale(1,-1);
 
       for (var x = 0; x < canvas.width; ++x) {
-        var amplitude = Math.max(-(this.floor - this.spectrum[x]), 0);
-        var height = amplitude / (-this.floor) * canvas.height
+        var amplitude = Math.max(-(this.generator.floor - this.generator.spectrum[x]), 0);
+        var height = amplitude / (-this.generator.floor) * canvas.height
         //console.log("amp: " + amplitude + ", h: " + height);
         ctx.fillRect(x, 0, 1, height);
       }
@@ -94,19 +70,19 @@ AnalyserCtrl.prototype = {
   },
 
   play: function() {
-    if (!this.playing) {
+    if (!this.generator.playing) {
       console.log("play");
-      this.playing = true;
-      this.sourceNode.noteOn(0);
+      this.generator.playing = true;
+      this.generator.sourceNode.noteOn(0);
       this.drawSpectrum();
     }
   },
 
   stop: function() {
-    if (this.playing) {
+    if (this.generator.playing) {
       console.log("stop");
-      this.playing = false;
-      this.sourceNode.noteOff(0);
+      this.generator.playing = false;
+      this.generator.sourceNode.noteOff(0);
     }
   }
 };
---
+++ js/services.js
@@ -0,0 +1,57 @@
+angular.service('generator', function() {
+  var svc = {
+    "bufferSize": 4096,
+    "floor": -60,
+    "playing": false,
+    "frequency": 440,
+    "functionName": "sine"
+  };
+
+  var functions = {
+    "sine": function(f, t) { return Math.sin(2*Math.PI*f*t)},
+    "square": function(f, t) { return (Math.sin(2*Math.PI*f*t)>0) ? 1 : -1 },
+    "triangle": function(f, t) { return 2*(Math.abs(f*t%1-0.5)-0.25) }
+  };
+
+  var audioContext = new webkitAudioContext();
+  svc.sampleRate = audioContext.sampleRate;
+  
+  // Calculate window, a Hanning Window
+  // TODO: consider a Kaiser-Bessel window
+  var windowCoeff = new Array(svc.bufferSize)
+  var mid = Math.floor(svc.bufferSize / 2);
+  windowCoeff[0] = 0;
+  windowCoeff[mid] = 1;
+  for (var i = 0; i < mid; ++i) {
+    windowCoeff[mid + i] = (1.0 + Math.cos(Math.PI * i / mid)) / 2;
+    windowCoeff[mid - i] = windowCoeff[mid + i]
+  }
+
+  // Create source
+  var sourceNode = audioContext.createBufferSource();
+  sourceNode.loop = true;
+  sourceNode.buffer = audioContext.createBuffer(1, svc.bufferSize, svc.sampleRate);
+
+  // Create analyser
+  var analyserNode = audioContext.createAnalyser();
+
+  // Wire things up
+  sourceNode.connect(analyserNode, 0, 0);
+  analyserNode.connect(audioContext.destination, 0, 0);
+
+  svc.sourceNode = sourceNode;
+  svc.analyserNode = analyserNode;
+  svc.samples = sourceNode.buffer.getChannelData(0);
+  svc.spectrum = new Float32Array(analyserNode.frequencyBinCount);
+
+  svc.setFunction = function(functionName) {
+    svc.functionName = functionName;
+    var fn = functions[functionName]
+    for (var x = 0; x < svc.samples.length; ++x) {
+      var t = x / svc.sampleRate;
+      svc.samples[x] = fn(svc.frequency, t) * windowCoeff[x];
+    }
+  }
+  return svc;
+
+}, {});
